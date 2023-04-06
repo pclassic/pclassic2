5431f9fe8d71c75545d0f33df5f8951f933d6337
ziwenhou018 <ziwenhou018@gmail.com>
2023-04-06 01:53:48 -0400

    Added parsing tutorial link

--- src/components/Parsing.js
+++ src/components/Parsing.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import Bar from '../Bar';
-import { CodeBlock } from "react-code-blocks";
+import { CodeBlock } from 'react-code-blocks';
 
 function Parsing() {
   return (
     <div>
-        <Bar />
-        <br />
-        <h1>Parsing </h1>
-        <a href="q0parsing.class">Class File</a>
-        <br />
-        <a href="q0parsing.cpp">C++ File</a>
-        <br />
-        <a href="q0parsing.java">Java File</a>
-        <br />
-        <a href="q0parsing.py">Python File</a>
+      <Bar />
+      <br />
+      <h1>Parsing 📝</h1>
+      <br />
+      <h6>
+        Parsing is actually very easy! Below are files tutorials you can peek at to learn how to
+        parse.
+      </h6>
+      <h6>
+        We have a{' '}
+        <a href="https://drive.google.com/file/d/1Dt1WtVxcyoVuRo1iFmHiw4Q1liCko0TF/view?fbclid=IwAR3IFZfQ9lxQW3KSK15l5ZX00uwWTdbFsrWDQQ4OAt3dUNc2geVNciafTmU">
+          video tutorial
+        </a>{' '}
+        here if you would like a walkthrough.
+      </h6>
+      <br />
+      <a href="q0parsing.class">Class File</a>
+      <br />
+      <a href="q0parsing.cpp">C++ File</a>
+      <br />
+      <a href="q0parsing.java">Java File</a>
+      <br />
+      <a href="q0parsing.py">Python File</a>
     </div>
-  )
+  );
 }
 
-export default Parsing
+export default Parsing;
