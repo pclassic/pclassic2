b86cb4662f7605c7ddee89b24769c3e4b9f394e3
Michael Gao <17tickles38@gmail.com>
2024-11-25 03:20:39 -0500

    add faqs, fix typo

--- src/components/FAQs.js
+++ src/components/FAQs.js
@@ -3,27 +3,35 @@ import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
 import { Heading } from '@chakra-ui/react';
 
+import entrancemap from "./images/entrancemap.png";
+
 function FAQs() {
   return (
     <Card style={{margin: '50px 120px'}}>
       <Card.Header className="header">
         <Heading fontWeight={"400"} fontSize={"3xl"} mt={1}>
-      🤔 Frequently Asked Questions
-      </Heading>
-                </Card.Header>
-        <Accordion flush style={{margin: "40px"}}>
+          🤔 Frequently Asked Questions
+        </Heading>
+      </Card.Header>
+      <p>For more details, check the <a href="https://www.pclassic.org/about">"About"</a> page.</p>
+      <Accordion flush style={{margin: "40px"}}>
       <Accordion.Item eventKey="0">
         <Accordion.Header>Where is the competition held?</Accordion.Header>
         <Accordion.Body>
         The PClassic Competition takes place at 107 Towne Bldg, Philadelphia, PA 19104. To enter, come 
-        to Levine Hall.
+        to Levine Hall.  Look for big glass doors; these will be the only ones unlocked.
+        <center><img
+          src={entrancemap}
+          alt="Contest Entrance Map"
+        /></center>
+        There is street parking in the vicinity, along Walnut, 33rd, and 34th Streets.
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="1">
         <Accordion.Header>Does an adult need to come?</Accordion.Header>
         <Accordion.Body>
         We ask that each school has an adult chaperone. That person doesn't need to 
-        be a teacher - it can be a parent or other involved person - but we need 
+        be a teacher—it can be a parent or other involved person—but we need 
         someone to be responsible for getting everyone here and then back home. 
         Please email us if this would be an issue; we've made exceptions for 
         special cases in the past.
@@ -39,8 +47,20 @@ function FAQs() {
       <Accordion.Item eventKey="3">
         <Accordion.Header>Will there be food?</Accordion.Header>
         <Accordion.Body>
-          Yes! We will provide free lunch and other snacks for our attendees to enjoy. 
+          Yes! We will provide free lunch, water, and other snacks for our attendees to enjoy. 
           We usually order ~100 boxes of pizza for each of our events!
+          Each team gets at least one.  There are also several boxes for coaches/chaperones.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="4">
+        <Accordion.Header>Will there be prizes?</Accordion.Header>
+        <Accordion.Body>
+          Each member of the top 3 advanced teams get a trophy and a prize.
+          Each member of the top 1 classic team gets a trophy
+          (We may give out trophies for the top 3 classic teams soon!).
+          Unfortunately, we do not give out prizes for the classic teams.
+          In the past, this has encouraged more overqualified advanced teams to do the classic division to obtain the prizes—
+          this is not in the spirit of the contest!
         </Accordion.Body>
       </Accordion.Item>
     </Accordion>
--- src/components/problems/s2022.js
+++ src/components/problems/s2022.js
@@ -14,7 +14,7 @@ PClassic had their second virtual competition, hosted on CodeChef [<a href="http
 took place on Saturday April 9th, 2022.
 <br/><br/>
 The contest theme this year was countries!<br/>
-1st place a trophy, a free PClassic shirt, and a prize item.<br/>
+1st place received a trophy, a free PClassic shirt, and a prize item.<br/>
 2nd place received a trophy and a free shirt.<br/>
 3rd place received a trophy.<br/>
 Prize item is a choice between: Amazon Echo Dot, Mechanical Keyboard, Steam Gift Card.<br/>
@@ -44,13 +44,14 @@ Congratulations to the winning teams!<br/>
 <p>We have a tech talk from 10-11am featuring University of Pennsylvania professor Chris Callison-Burch! He teaches and researches Artificial Intelligence and Computational Linguistics. The topic of the tech talk will be about Artificial Intelligence! </p>
 
 <p><a href="https://upenn.zoom.us/j/99009027227">Zoom</a> <b>10:00 am - 11:00 am</b>: Tech Talk with Professor Callison-Burch</p>
-<p><a href="https://youtu.be/ly5YqoD5jlg">YouTube</a> <b>11:00 am - 12:00 pm</b>: <a href="https://docs.google.com/presentation/d/1cfjMIq1rb4Kvy6GBFbn-fhon5nnOddaJJ263R_auBIc/edit?usp=sharing"></a>Opening</p>
+<p><a href="https://youtu.be/ly5YqoD5jlg">YouTube</a> <b>11:00 am - 12:00 pm</b>: <a href="https://docs.google.com/presentation/d/1cfjMIq1rb4Kvy6GBFbn-fhon5nnOddaJJ263R_auBIc/edit?usp=sharing">Opening</a></p>
 <p>[<a href="https://www.codechef.com/PCSN2022">Novice]</a> [<a href="https://www.codechef.com/PCSS2022">Standard</a>] <b>12:00 pm - 4:00 pm</b>: Contest on CodeChef</p>
 <p><a href="https://upenn.zoom.us/j/95230762623">Zoom</a> <b>12:00 pm - 4:00 pm</b>: Contest Help Desk</p>
 <p><a href="https://www.youtube.com/watch?v=-fE4vLZ313o">YouTube</a> <b>4:00 pm - 4:30 pm</b>: Closing - Q+A</p>
 <p><a href="https://youtu.be/1toOqr2cLMc">YouTube</a> <b>4:30 pm - 4:45 pm</b>: <a href="https://docs.google.com/presentation/d/1VVKGQVTsHW2xVmpfA-VeFXAP-6zh0HSRymmFpI4IOGA/edit#slide=id.g123b2fd57c2_0_1775">Closing</a> - Winners</p>
 
-<p>Other links: <a href="https://tinyurl.com/pclsp22shirts">shirts</a> <a href="https://tinyurl.com/pclsp22feedback">feedback</a></p>
+<p>Other links: <a href="https://tinyurl.com/pclsp22feedback">feedback</a></p>
+{/* <a href="https://tinyurl.com/pclsp22shirts">shirts</a>  */}
 
 
     </div>
