6eeb84b43e80b70614e54e77bd7bf6ff328c6558
Eric Zou <ezou626@gmail.com>
2026-03-22 17:55:33 -0400

    remove broken link

--- src/components/Parsing.js
+++ src/components/Parsing.js
@@ -13,29 +13,23 @@ function Parsing() {
       <Bar />
       <br />
       <Heading fontSize={"3xl"} fontWeight={400}>Parsing 🤖</Heading>
-      <br />
-      <Text fontSize={"2xl"} color={"#566573"} fontFamily={`'Lato', sans-serif;`}>
-        Parsing is easy! Check out our Parsing tutorial and sample files below:
-      </Text>
-       
-      <br />
       <Container>
       <Row>
-      <Col>
-      <i>Please check again later for a video guide.</i>
-      <iframe src="https://drive.google.com/file/d/1Dt1WtVxcyoVuRo1iFmHiw4Q1liCko0TF/preview" width="900" height="480" allow="autoplay"></iframe>
-      </Col>
-      <Col>
-      <Card>
-      <Card.Header color="grey">Sample Parsing Files 🔗</Card.Header>
-      <ListGroup>
-      <ListGroup.Item action href="q0parsing.class">Class File</ListGroup.Item>
-      <ListGroup.Item action href="q0parsing.cpp">C++ File</ListGroup.Item>
-      <ListGroup.Item action href="q0parsing.java">Java File</ListGroup.Item>
-      <ListGroup.Item action href="q0parsing.py">Python File</ListGroup.Item>
-    </ListGroup>
-    </Card>
-      </Col>
+        <Text fontSize={"2xl"} color={"#566573"} fontFamily={`'Lato', sans-serif;`}>
+          Parsing data from standard input and printing to standard output are common tasks in competitive programming, and being comfortable with it can save you a lot of time during contests.
+        If you're new to this, check out some examples for how to do it in different languages!
+        </Text>
+      </Row>
+      <Row>
+        <Card>
+          <Card.Header color="grey">Sample Parsing Files 🔗</Card.Header>
+          <ListGroup>
+            <ListGroup.Item action href="q0parsing.class">Class File</ListGroup.Item>
+            <ListGroup.Item action href="q0parsing.cpp">C++ File</ListGroup.Item>
+            <ListGroup.Item action href="q0parsing.java">Java File</ListGroup.Item>
+            <ListGroup.Item action href="q0parsing.py">Python File</ListGroup.Item>
+          </ListGroup>
+        </Card>
       </Row>
       </Container>
       <br />
