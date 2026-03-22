7452ac64ac8e9547d149e7bfc6bddb424f19c7c3
Eric Zou <ezou626@gmail.com>
2026-03-22 19:29:49 -0400

    move organizer data to public

--- src/components/Organizers.js
+++ src/components/Organizers.js
@@ -6,51 +6,9 @@ import Row from 'react-bootstrap/Row';
 import Nav from "react-bootstrap/Nav"
 import { LinkContainer } from 'react-router-bootstrap';
 import { Text, Heading } from '@chakra-ui/react';
-
-import michael from './images/michael.jpg';
-import kurtis from "./images/kurtis.png";
-import ntarsis from "./images/ntarsis.jpg";
+import organizers from './organizersData.json';
 
 function Organizers() {
-  const organizers = [
-    {
-      photo: michael,
-      name: 'Michael Gao',
-      coLead: true,
-      description:
-        "\"I am a junior studying computer science.  I like (in decreasing order of skill) wasting time, video games, badminton, playing piano, and poker.\"",
-    },
-    {
-      photo: kurtis,
-      name: 'Kurtis Zhang',
-      coLead: true,
-      description:
-        "Kurtis is a freshman studying Computer Science at UPenn. Outside of school he enjoys skiing, playing piano and watching football.",
-    },
-    {
-      photo: ntarsis,
-      name: 'Nicholas Tarsis',
-      coLead: true,
-      description:
-        "\"I bench 315.\"",
-    },
-    {
-      photo: process.env.PUBLIC_URL + '/p.jpg',
-      name: 'Shaan Keole',
-      description: '',
-    },
-    {
-      photo: process.env.PUBLIC_URL + '/p.jpg',
-      name: 'Eric Zou',
-      description: '',
-    },
-    {
-      photo: process.env.PUBLIC_URL + '/p.jpg',
-      name: 'Pragya Singh',
-      description: '',
-    },
-  ];
-
   return (
     <div>
       <Bar />
@@ -63,21 +21,21 @@ function Organizers() {
       </LinkContainer>
       <Row xs={1} md={3} className="g-6" style={{ padding: '10px 90px' }}>
         {organizers.map((organizer) => (
-          <Col>
+          <Col key={organizer.name}>
             <Card border="light" style={{ padding: '20px', borderRadius: '15px' }}>
               <Card.Img
                 style={{ width: '100%', height: '30vw', objectFit: 'cover' }}
                 variant="top"
-                src={organizer.photo}
+                src={process.env.PUBLIC_URL + '/' + organizer.photo}
               />
               <Card.Body>
                 <Card.Title>
                   <Heading fontSize={"lg"} fontWeight={"700"}>{organizer.name}</Heading>
                   {organizer.coLead && <Heading fontSize={"sm"} fontWeight={"400"} color={"#566573"}>Co-Lead</Heading>}
                 </Card.Title>
-                  <Text color={"#566573"} textAlign={"left"} fontFamily={`'Lato', sans-serif;`}>
-                    {organizer.description}
-                  </Text>
+                <Text color={"#566573"} textAlign={"left"} fontFamily={`'Lato', sans-serif;`}>
+                  {organizer.description}
+                </Text>
               </Card.Body>
             </Card>
           </Col>
--- src/components/PastOrganizers.js
+++ src/components/PastOrganizers.js
@@ -1,96 +1,45 @@
-import React from 'react'
-import Bar from '../Bar'
+import React from 'react';
+import Bar from '../Bar';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-
-import ani from './past/ani.jpg'
-import brandon from './past/brandon.jpg'
-import eric from './past/eric.jpg'
-import erik from './past/erik.jpg'
-import pedro from './past/pedro.jpg'
-import roger from './past/roger.jpg'
-import stephanie from './past/stephanie.png'
-import vatsin from './past/vatsin.jpg'
-import meggie from './past/meggie.jpg'
-import james from './past/james.jpg'
-import ying from './past/ying.jpg'
-
-import ben from './past/ben.jpg'
-import brian from './past/brian.jpg'
-import michelle from './past/Michelle.jpg'
-import kyle from './past/kyle.jpg'
-import steven from './past/Steven.jpg'
-import ethan from './past/echee9.jpg';
-import ishaan from './past/ishaan.jpg';
-import grace from './past/grace.jpg';
-import hemosoo from './past/hemosoo.jpg';
-import michaelli from "./past/michaelli.jpg";
-
-import ziwen from "./past/ziwen.jpg";
-import tien from "./past/tien.jpg";
-
-import jason from "./images/jason.png";
-import kevin from "./images/kevin.jpg";
-import aaron from "./images/Aaron.JPG";
-import nick from "./images/Nick.JPG";
-import aaronWu from "./images/aaronWu.jpg";
+import Nav from 'react-bootstrap/Nav';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Heading } from '@chakra-ui/react';
+import organizers from './pastOrganizersData.json';
 
 function PastOrganizers() {
-    const organizers = [
-        // {photo: , name: "", description: ""}, //
-        {photo: jason, name: "Jason Liu", description: "\"I do computer science.\""},
-        {photo: kevin, name: "Kevin Liu", description: "Kevin Liu is a sophomore studying CIS. He enjoys sleeping in on the weekends."},
-        {photo: aaron, name: "Aaron Mei", description: "Aaron Mei is a CIS major at Penn who enjoys playing video games and thinking about algorithms."},
-        {photo: nick, name: "Nick Eiffert", description: "Nicholas Eiffert is a junior studying mathematics and computer science in CAS. He went to Bordentown Regional High School. He is a TA for a data structures and algorithms course at Penn. He likes doing competitive programming, going to concerts, and eating late-night ramen."},
-        {photo: aaronWu, name: "Aaron Wu", description: "\"I watch anime.\""},
-        {photo: hemosoo, name: 'Hemosoo Woo', description: "(23fa-24sp)\nHemosoo is a freshman studying computer science at UPenn. In his free time, he enjoys playing sports such as soccer or basketball, hitting the gym, playing chess, and listening to music of all sorts of genres."},
-        {photo: michaelli, name: 'Michael Li', description: "(24sp)\nMichael is a freshman studying CS with a focus in software development and full stack development. He's a UFC fan and always loves seeing an underdog win!"},
-        {photo: ishaan, name: "Ishaan Lal", description: ""}, // Ishaan is a senior studying Computer Science. This is his sixth semester in the club and fourth as the co-lead. In his free time, he enjoys teaching, reading, and following chess.
-        {photo: ethan, name: "Ethan Chee", description: ""}, // Ethan Chee is a senior studying CS. In his free time he likes to rock climb, run and play video games.
-        {photo: grace, name: "Grace Lee", description: ""}, // Grace is a senior in Computer Science. She typically spends her time working on problem sets, drawing, or talking about cartoons.
-        {photo: kyle, name: "Kyle Xiong", description: ""}, // Kyle is a senior studying computer science at UPenn. Hailing from Connecticut, he enjoys basketball, skiing, io games, and sometimes computer science. He also wishes you luck on your coding endeavors!
-        {photo: michelle, name: "Michelle Chang", description: ""}, // Michelle Chang is a sophomore studying CS. She likes web development, K-Pop dancing, and running
-        {photo: steven, name: "Steven Chang", description: ""}, // Steven Chang is a freshman studying computer science from Myanmar. In high school, he participated in math olympiads and volunteering activities. Some of his hobbies are watching K-Dramas, playing chess, and solving the Rubik's cube.
-        {photo: ben, name: "Benjamin Le", description: ""}, // Benjamin Le is a sophomore studying comdputer science in SEAS. He enjoys learning mathematics and solving interesting problems. In his free time, he likes to read and play video games.
-        {photo: ziwen, name: "Ziwen Hou", description: ""},
-        {photo: brian, name: "Brian Chen", description: ""}, // Brian Chen is a master's student studying computer science. He studied computational biology and computer science as an undergraduate. He enjoys playing basketball, solving Rubik's cubes, and learning about languages.
-        {photo: ani, name: "Ani Machaidze", description: "Ani Machaidze is a senior studying Computer Science with a minor in Engineering Entrepreneurship. She is from Tbilisi, Georgia. During high-school she competed in several national programming competitions in Georgia. In her free time, she enjoys photography, playing ping-pong, and watching TV shows."},
-        {photo: brandon, name: "Brandon Lin", description: "Brandon Lin was a 2020 alumni who studied computer science and data science in SEAS. He likes coding in Python and learning about mathematics and enjoys solving Rubik's Cubes, singing & playing music, and making origami."},
-        {photo: eric, name: "Eric Wang", description: "Eric Wang was a 2020 alumni who studied computer science. In his free time, Eric enjoys coding and contributing to open source projects."},
-        {photo: erik, name: "Erik Zhao", description: "Erik Zhao was a 2019 alumni who studied computer science and robotics in SEAS. In his free time, he enjoys ice skating, crosswording, and playing strategy games."},
-        {photo: pedro, name: "Pedro Sacramento", description: "Pedro Sacramento was a 2021 alumni who studied math and computer science in SEAS. His favorite programming language is C++ (by far), he likes reading, cats and watching tons of YouTube videos."},
-        {photo: roger, name: "Roger Zhang", description: "Roger Zhang was a 2018 alumni who studied computer science. He likes to use the computer science knowledge he learns from the classroom to make really useless programs with Java and Python. Outside of programming, he enjoys playing guitar hero and hanging out with friends."},
-        {photo: stephanie, name: "Stephanie Shi", description: "Stephanie Shi was a 2020 alumni who studied Computer Science and Statistics in the Jerome Fisher Program in Management and Technology. Stephanie was a TA for CIS 160, one of Penn’s introductory computer science classes, and the Education Chair for Hack4Impact. Stephanie is passionate about design, math, and instant ramen."},
-        {photo: vatsin, name: "Vatsin Suchak", description: "Vatsin Suchak is a senior at Penn, currently studying computer science and math. He is a TA for a discrete math course at Penn, CIS 160. He also enjoys watching motorsports, playing the guitar, and loves dogs."},
-        {photo: meggie, name: "Meggie Cheng", description: "Meggie Cheng is a senior in Digital Media Design in SEAS. She was born in NYC but was raised in Houston, Texas and went to Cypress Woods High School. Her hobbies include competitive programming (did in high school), digital painting, reading webtoons."},
-        {photo: tien, name: "Tien Pham", description: ""},
-        {photo: james, name: "James Hu", description: "James Hu was a 2020 alumni who studied Computer Science with minors in Mathematics and Statistics. He was on the Penn rugby team. He also enjoys camping, skateboarding, and playing video games with friends."},
-        {photo: ying, name: "Yingxuan Eng", description: "Yingxuan Eng is a senior studying computer science in SEAS. He enjoys mathematics as well as writing code to solve problems from various disciplines. In his free time, he likes to read, binge-watch TV series, and play Smash."},
-      ];
-
   return (
     <div>
-        <Bar/>
-        <br/>
-        <h2>Past Organizers 🧐 </h2>
-        <Row xs={1} md={3} className="g-6" style={{padding: "10px 90px"}}>
-      {organizers.map((organizer) => (
-        <Col>
-          <Card border="light" style={{padding: "20px", borderRadius: "15px"}}>
-            <Card.Img style={{width: "100%", height: "30vw", objectFit: "cover"}} variant="top" src={organizer.photo} />
-            <Card.Body>
-              <Card.Title>{organizer.name}</Card.Title>
-              <Card.Text style={{textAlign: "left"}}>
-                {organizer.description}
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
-    </Row>
+      <Bar />
+      <br />
+      <Heading fontSize={"3xl"} fontWeight={400}>Past Organizers 🧐</Heading>
+      <LinkContainer to="/organizers">
+        <Nav.Link>
+          <u>Current Organizers</u>
+        </Nav.Link>
+      </LinkContainer>
+      <Row xs={1} md={3} className="g-6" style={{ padding: '10px 90px' }}>
+        {organizers.map((organizer) => (
+          <Col key={organizer.name}>
+            <Card border="light" style={{ padding: '20px', borderRadius: '15px' }}>
+              <Card.Img
+                style={{ width: '100%', height: '30vw', objectFit: 'cover' }}
+                variant="top"
+                src={process.env.PUBLIC_URL + '/' + organizer.photo}
+              />
+              <Card.Body>
+                <Card.Title>{organizer.name}</Card.Title>
+                <Card.Text style={{ textAlign: 'left' }}>
+                  {organizer.description}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </div>
-  )
+  );
 }
 
-export default PastOrganizers
+export default PastOrganizers;
