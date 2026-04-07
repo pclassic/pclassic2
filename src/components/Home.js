d26f5ccf1e07e28cfbf7c656958e16b9217467d9
Eric <55918461+ezou626@users.noreply.github.com>
2026-04-07 19:41:11 -0400

    Convert comment to JSX syntax in Home.js
    Updated comment to JSX syntax for Spring 2026 PClassic competition.

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,17 +16,17 @@ function Home() {
       {show && <Alert variant="success" onClose={() => setShow(false)} dismissible>
           <Heading fontSize={"xl"}>🚨SPRING 2026 IS CANCELLED!🚨</Heading>
           <p>
-            // We're hosting the Spring 2026 PClassic competition! Please sign up&nbsp;
-            // <a 
-            //   id="signUpLink"
-            //   title="Sign Up Link"
-            //   href={SP26_SIGN_UP_LINK}
-            //   rel="noreferrer noopener nofollow"
-            //   class='underline'
-            // >
-            //   here
-            // </a>
-            // !
+  {/*We're hosting the Spring 2026 PClassic competition! Please sign up&nbsp;
+            <a 
+              id="signUpLink"
+              title="Sign Up Link"
+              href={SP26_SIGN_UP_LINK}
+              rel="noreferrer noopener nofollow"
+              class='underline'
+            >
+              here
+            </a>
+            !*/}
             Due to logistical concerns, the Spring 2026 PClassic Competition has been cancelled. See you in the fall!
           </p>
         </Alert>}
